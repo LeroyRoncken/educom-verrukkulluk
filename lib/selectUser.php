52ff1084c5c950d39286b090560a0be3e8acbbce
LeroyRoncken <leroy.roncken@gmail.com>
2025-01-02 15:52:28 +0100

    User Class (#6)

---
+++ lib/selectUser.php
@@ -0,0 +1,22 @@
+<?php
+
+class user {
+
+    private $connection;
+
+    public function __construct($connection) {
+        $this->connection = $connection;
+    }
+
+    public function selectUser($user_id) {
+
+        $sql = "SELECT * FROM user WHERE id = $user_id";
+
+        $result = mysqli_query($this->connection, $sql);
+        $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+        return($user);
+
+    }
+
+}

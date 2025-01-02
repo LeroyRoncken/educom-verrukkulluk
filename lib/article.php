259654b49993b7b019e3e90aa6d2cc8d6f8624ab
LeroyRoncken <leroy.roncken@gmail.com>
2025-01-02 15:52:07 +0100

    Article Class (#5)

---
+++ lib/article.php
@@ -0,0 +1,22 @@
+<?php
+
+class article {
+
+    private $connection;
+
+    public function __construct($connection) {
+        $this->connection = $connection;
+    }
+  
+    public function selectArticle($article_id) {
+
+        $sql = "SELECT * FROM article WHERE id = $article_id";
+        
+        $result = mysqli_query($this->connection, $sql);
+        $article = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+        return($article);
+        
+    }
+
+}

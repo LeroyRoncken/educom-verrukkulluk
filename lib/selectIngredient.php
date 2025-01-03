a58b78055798cfa4424c1bd6d7d945e3b41194dc
LeroyRoncken <leroy.roncken@gmail.com>
2025-01-03 17:02:50 +0100

    Ingredient class (#9)

---
+++ lib/selectIngredient.php
@@ -0,0 +1,42 @@
+<?php
+
+class ingredient {
+
+    private $connection;
+    private $art;
+
+    public function __construct($constructConnection) {
+        $this->connection = $constructConnection;
+        $this->art = new article($constructConnection);
+    }
+
+    private function selectArticle($article_id) {
+        //article.selectArticle($article_id);
+        $selArt = $this->art->selectArticle($article_id);
+        return($selArt);
+    }
+
+    public function selectIngredient($dish_id) {
+
+        $sql = "SELECT * FROM ingredient WHERE dish_id = $dish_id";
+        $result = mysqli_query($this->connection, $sql);
+
+        $return = [];
+
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            $art_id = $row["article_id"];
+            $ingrArt = $this->selectArticle($art_id);
+        
+            $return[] = [
+                "id" => $row["id"],
+                "dish_id" => $row["dish_id"],
+                "article_id" => $art_id,
+                "name" => $ingrArt["name"],
+                "description" => $ingrArt["description"]
+            ];
+        }
+
+        return($return);
+
+    }
+}

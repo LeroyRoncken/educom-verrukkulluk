a6ced460fc9fd041b02d91534aba37aeff47e48b
LeroyRoncken <leroy.roncken@gmail.com>
2025-01-07 10:33:08 +0100

    addFavorite en deleteFavorite toegevoegd (#8)

--- lib/selectInfo.php
+++ lib/selectInfo.php
@@ -10,30 +10,26 @@ class dishInfo {
         $this->user = new user($constructConnection);
     }
 
-    private function selectUser($user_id) {
-        $selUser = $this->user->selectUser($user_id);
-        return($selUser);
+    public function addFavorite($dish_id, $user_id) {
+        $sql = "INSERT INTO dish_info (record_type, dish_id, user_id) VALUES ('F', $dish_id, $user_id)";
+        mysqli_query($this->connection, $sql);
+    }
+
+    public function deleteFavorite($dish_id, $user_id) {
+        $sql = "DELETE FROM dish_info WHERE record_type = 'F' AND dish_id = $dish_id AND user_id = $user_id";
+        mysqli_query($this->connection, $sql);
     }
 
     public function selectInfo($dish_id, $record_type) {
         
-        $sql = "SELECT * FROM dish_info WHERE dish_id = $dish_id AND record_type = '$record_type'"; // Use single quotes for an input that is a string (i.e. record type is given in a letter).
+        $sql = "SELECT * FROM dish_info WHERE dish_id = $dish_id AND record_type = '$record_type'";
+        // Use single quotes for an input that is a string (i.e. record type is given in a letter).
 
         $result = mysqli_query($this->connection, $sql);
 
-        $baseArray = [];
         $return = [];
 
         while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-
-            // $baseArray = [
-            //     "id" => $row["id"],
-            //     "record_type" => $row["record_type"],
-            //     "dish_id" => $row["dish_id"],
-            //     "date" => $row["date"],
-            //     "numerical_field" => $row["numerical_field"],
-            //     "text_field" => $row["text_field"]
-            // ];
 
             $fetchUser = [];
             
@@ -42,52 +38,17 @@ class dishInfo {
                 $user_id = $row["user_id"];
                 $fetchUser = $this->selectUser($user_id);
 
-                // $userArray = [
-                //     "user_id" => $row["user_id"],
-                //     "name" => $fetchUser["name"]
-                // ];
-                
-                // $return [] = [...$baseArray, ...$userArray];
-
             }
 
             $return[] = [...$fetchUser, ...$row];
-            
         }
 
         return($return);
+    }
 
+    private function selectUser($user_id) {
+        $selUser = $this->user->selectUser($user_id);
+        return($selUser);
     }
 
 }
-
-
-            // $return[] = [
-            //     "id" => $row["id"],
-            //     "record_type" => $row["record_type"],
-            //     "dish_id" => $row["dish_id"]
-            // ];
-
-            // if ($row["user_id"] != NULL) {
-            //     $user_id = $row["user_id"];
-            //     $fetchUser = $this->selectUser($user_id);
-            //     $return[] = [
-            //         "user_id" => $row["user_id"],
-            //         "name" => $fetchUser["name"]
-            //     ];
-            // }
-            // if ($row["date"] != NULL) {
-            //     $return[] = [
-            //         "date" => $row["date"]
-            //     ];
-            // }
-            // if ($row["numerical_field"] != NULL) {
-            //     $return[] = [
-            //         "numerical_field" => $row["numerical_field"]
-            //     ];
-            // }
-            // if ($row["text_field"] != NULL) {
-            //     $return[] = [
-            //         "text_field" => $row["text_field"]
-            //     ];
-            // }

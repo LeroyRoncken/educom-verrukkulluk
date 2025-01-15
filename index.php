85e6510fa3c79ce6093f0d7ce37d884dab655e0f
LeroyRoncken <leroy.roncken@gmail.com>
2025-01-15 13:16:59 +0100

    Footer en gedeeltelijke Header (#13)

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
+//// Allereerst zorgen dat de "Autoloader" uit vendor opgenomen wordt:
+require_once("./vendor/autoload.php");
 
+/// Twig koppelen:
+$loader = new \Twig\Loader\FilesystemLoader("./templates");
+/// VOOR PRODUCTIE:
+/// $twig = new \Twig\Environment($loader), ["cache" => "./cache/cc"]);
+
+/// VOOR DEVELOPMENT:
+$twig = new \Twig\Environment($loader, ["debug" => true ]);
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+
+/******************************/
+
+/// Next step, iets met je data doen. Ophalen of zo
 require_once("lib/database.php");
 require_once("lib/article.php");
 require_once("lib/selectUser.php");
@@ -7,50 +21,49 @@ require_once("lib/selectKitchenType.php");
 require_once("lib/selectIngredient.php");
 require_once("lib/selectInfo.php");
 require_once("lib/selectDish.php");
+require_once("lib/groceryListUserFriendly.php");
 
-
-/// INIT
 $db = new database();
+$dish = new dish($db->getConnection());
 // $art = new article($db->getConnection());
 // $user = new user($db->getConnection());
 // $kt = new kitchenType($db->getConnection());
 // $ingr = new ingredient($db->getConnection());
 // $info = new dishInfo($db->getConnection());
-$dish = new dish($db->getConnection());
+// $groce = new groceryList($db->getConnection());
+
+/*
+URL:
+http://localhost/educom-verrukkulluk/index.php?gerecht_id=4&action=detail
+*/
+
+$dish_id = isset($_GET["dish_id"]) ? $_GET["dish_id"] : "";
+$action = isset($_GET["action"]) ? $_GET["action"] : "homepage";
+
+switch($action) {
+
+    case "homepage": {
+        $data = $dish->selectDish();
+        $template = 'homepage.html.twig';
+        $title = "homepage";
+        break;
+    }
+
+    case "detail": {
+        $data = $dish->selectDish($dish_id);
+        $template = 'detail.html.twig';
+        $title = "detail page";
+        break;
+    }
+
+    /// etc
+
+}
+
+/// Onderstaande code schrijf je idealiter in een layout klasse of iets dergelijks
+/// Juiste template laden, in dit geval "homepage"
+$template = $twig->load($template);
 
 
-/// VERWERK 
-// $dataArt = $art->selectArticle(1);
-// $dataUser = $user->selectUser(1);
-// $dataKT = $kt->selectKitchenType(1);
-// $dataIngr = $ingr->selectIngredient(1);
-// $dataInfo = $info->selectInfo(1, 'O');
-// $addFavorite = $info->addFavorite(1,1);
-// $deleteFavorite = $info->deleteFavorite(1,1);
-$dataDish = $dish->selectDish();
-
-
-/// RETURN
-// echo "<pre>";
-// echo "Article data: ";
-// var_dump($dataArt);
-
-// echo "<pre>";
-// echo "User data: ";
-// var_dump($dataUser);
-
-// echo "<pre>";
-// echo "Kitchen Type data: ";
-// var_dump($dataKT);
-
-// echo "<pre>";
-// echo "Ingredient data: ";
-// var_dump($dataIngr);
-
-// echo "<pre>";
-// echo "Dish info: ";
-// var_dump($dataInfo);
-
-echo "<pre>";
-echo "Dish info: ";
-var_dump($dataDish);
+/// En tonen die handel!
+echo $template->render(["title" => $title, "data" => $data]);

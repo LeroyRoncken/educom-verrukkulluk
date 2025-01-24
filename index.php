b106388155673ba516e354bde583084706a48683
LeroyRoncken <leroy.roncken@gmail.com>
2025-01-24 10:18:21 +0100

    Rating - basis (#14)

--- index.php
+++ index.php
@@ -29,15 +29,19 @@ $dish = new dish($db->getConnection());
 // $user = new user($db->getConnection());
 // $kt = new kitchenType($db->getConnection());
 // $ingr = new ingredient($db->getConnection());
-// $info = new dishInfo($db->getConnection());
+$info = new dishInfo($db->getConnection());
 // $groce = new groceryList($db->getConnection());
+
+// $deleteAllRatings = $info->deleteRating();
 
 /*
 URL:
-http://localhost/educom-verrukkulluk/index.php?dish_id=4&action=detail
+    http://localhost/educom-verrukkulluk/index.php?dish_id=4&action=detail
+    http://localhost/educom-verrukkulluk/index.php?dish_id=4&rating_value=3&action=rating
 */
 
 $dish_id = isset($_GET["dish_id"]) ? $_GET["dish_id"] : "";
+$rating_value = isset($_GET["rating_value"]) ? $_GET["rating_value"] : "";
 $action = isset($_GET["action"]) ? $_GET["action"] : "homepage";
 
 switch($action) {
@@ -54,6 +58,29 @@ switch($action) {
         $template = 'detail.html.twig';
         $title = "detail page";
         break;
+    }
+
+    case "rating": {
+        header('Content-type: application/json');
+
+        // Add new rating to database
+        $info->addRating($dish_id, $rating_value);
+
+        //  Calculate average
+        $count = 0;
+        $total = 0;
+        $ratings = $info->selectInfo($dish_id, 'R');
+        foreach($ratings as $rating) {
+            $count ++;
+            $total += $rating["numerical_field"];
+        }
+        $average = $total / $count;
+
+        $output = array("success"=>true, "average"=>$average);
+        echo json_encode($output);
+
+        die();
+        // break;
     }
 
     /// etc
